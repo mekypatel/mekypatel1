53ca4ae0158b6e4bd24c37aec491e968dbba5e90
mekypatel <129590653+mekypatel@users.noreply.github.com>
2023-04-20 13:41:31 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,105 @@
+header = {
+        "LICENCE/AUTHORISATION/SCRIP": "",
+        "Name of Office issuing Authorisation": "",
+        "Name": "",
+        "Address of Applicant": "",
+        "IEC": "",
+        "Exporter Type": "",
+        "Exporter Status": "",
+        "Transferable/Actual User": "",
+        "File Number": "",
+        "Licence/Authorisation/Scrip No.": "",
+        "Date of Issue": "",
+        "FOB Value (In Rs.)": "",
+        "FOB Value (In US$)": "",
+        "CIF in Rs.": "",
+        "CIF in FFE": "",
+        "Port of Registration": "",
+        "Period of Shipment (Export Obligation Period)": "",
+        "Validity of Authorisation / Scrip for Import": "",
+        "Custom Notification Number": "",
+        "Custom Notification Date": "",
+        "Currency Area": "",
+        "Utilization/Transferability": ""
+}
+
+part1 = {
+                "Sl.No": [],
+                "ITCHS Code": [],
+                "Export Item Name": [],
+                "Qty": [],
+                "UOM": [],
+                "FOB/FOR(Rs.)": [],
+                "FOB/FOR(In currency of realisation)": []
+                
+}
+
+part2 = {
+                "Sl.No": [],
+                "ITCHS Code": [],
+                "Import Item Name": [],
+                "Qty": [],
+                "UOM": [],
+                "CIF (Rs.)": []
+}
+
+part3 = {
+                "Sl.No": [],
+                "Name": [],
+                "Address": [],
+                "Type of Unit": [],
+                "Reg. No.": [],
+                "Address of the jurisdictional Central Excise Authority": []
+}
+
+part4 = {
+                "Name and Address of the Co-Licensee": []
+}
+part5 = {
+                "CONDITION SHEET": []
+}
+
+
+part1_format =[ {
+    "Name":"",
+    "Items": {
+        "Sl.No":"",
+        "ITCHS Code": "",
+        "Export Item Name": "",
+        "Qty.": "",
+        "UOM": "",
+        "FOB/FOR(Rs.)": "",
+        "FOB/FOR(In currency of realisation)": ""
+    }
+}]
+part2_format =[ {
+    "Name":"",
+    "Items": {
+        "Sl.No": "",
+        "ITCHS Code": "",
+        "Import Item Name": "",
+        "Qty": "",
+        "UOM": "",
+        "CIF (Rs.)": ""
+    }
+}]
+
+part3_format =[ {
+    "Name":"",
+    "Items": {
+             "Sl.No": "",
+             "Name": "",
+            "Address": "",
+             "Type of Unit": "",
+             "Reg. No.": "",
+              "Address of the jurisdictional Central Excise Authority": ""
+    }
+}]
+
+
+part4_format =[ {
+                "Name and Address of the Co-Licensee": ""
+}]
+part5_format =[ {
+                "CONDITION SHEET": ""
+}]
